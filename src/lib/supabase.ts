a36349400b213a1007f1d9dd94cb112d688a9192
phabdelmegedamin-arch <phabdelmegedamin@gmail.com>
2026-04-03 14:26:25 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -41,7 +41,7 @@ export function createAdminClient() {
     throw new Error('Admin client cannot be used in browser')
   }
   const { createClient: createSBClient } = require('@supabase/supabase-js')
-  return createSBClient<Database>(
+   return createSBClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     { auth: { persistSession: false } }
